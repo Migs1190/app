e55c192327fe718b82a6b9993bc1b99c84745e65
Migs1190 <ahmad1372002@gmail.com>
2022-12-28 07:44:00 +0200

    tried lambda

--- src/main/java/dark/App.java
+++ src/main/java/dark/App.java
@@ -1,5 +1,7 @@
 package dark;
 
+import java.util.logging.Handler;
+
 import javafx.application.Application;
 import javafx.event.*;
 import javafx.scene.*;
@@ -9,24 +11,25 @@ import javafx.stage.*;
 
 public class App extends Application
 {
-    public static void main (String[] args)
-    {
-        launch (args);
-    }
+	Button butt;
+		public static void main (String[] args)
+		{launch (args);}
 
-    @Override
-    public void start(Stage primaryStage) throws Exception
-    {
-        primaryStage.setTitle("Windoo");
+		@Override
+		public void start(Stage primaryStage) throws Exception
+		{
+				primaryStage.setTitle("Windoo");
 
 
-        Button butt = new Button("Butt");
+				Button butt = new Button("Click me");
+				butt.setOnAction(e -> System.out.println("Button clicked"));
 
-        StackPane layout = new StackPane();
-        layout.getChildren().add(butt);
+				StackPane layout = new StackPane();
+				layout.getChildren().add(butt);
 
-        Scene scene = new Scene(layout, 600, 600);
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
+				Scene scene = new Scene(layout, 600, 600);
+				primaryStage.setScene(scene);
+				primaryStage.show();
+		}
+
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module dark {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.logging;
 
     opens dark to javafx.fxml;
     exports dark;
